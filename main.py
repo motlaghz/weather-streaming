835af47d550e969b1e37fc9a819c4dce90101f32
Sanam <ghodsiyeh.motlaghzadeh@helsinki.fi>
2025-08-25 15:12:52 +0300

    Display time of forecast

--- main.py
+++ main.py
@@ -5,14 +5,15 @@ from plotting import plot_all_parameters
 
 def run_pipeline():
     while True:
-        # Download global and regional forecasts
-        download_forecast("forecast_globe.grib")
-        download_forecast("forecast_nordic.grib", area="72/-25/54/40")
+        # download and get download-time fallback
+        _, download_time_globe = download_forecast("forecast_globe.grib")
+        _, download_time_nordic = download_forecast("forecast_nordic.grib", area="72/-25/54/40")
 
-        for file_name in ["forecast_nordic.grib", "forecast_globe.grib"]:
-            label = file_name.split("_")[-1].split(".")[0]  # gets 'nordic' or 'globe'
+        for file_name, download_time in [("forecast_nordic.grib", download_time_nordic),
+                                        ("forecast_globe.grib", download_time_globe)]:
+            label = file_name.split("_")[-1].split(".")[0]
             with xr.open_dataset(file_name, engine="cfgrib") as ds:
-                plot_all_parameters(ds, label)
+                plot_all_parameters(ds, label, time_override=download_time)
 
         # Wait before next update (e.g., 1 hour)
         time.sleep(3600)
