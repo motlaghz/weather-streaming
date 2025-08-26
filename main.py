ce71067aae41e5e64e349735e775de6ff0c5652d
Sanam <ghodsiyeh.motlaghzadeh@helsinki.fi>
2025-08-26 16:16:38 +0300

    Get one global dataset only and run the plotting two times

--- main.py
+++ main.py
@@ -5,14 +5,13 @@ from plotting import plot_all_parameters
 
 def run_pipeline():
     while True:
-        # Download global and regional forecasts
+        # Download global forecasts
         _ , _ = download_latest_run("forecast_globe.grib")
-        _ , _ = download_latest_run("forecast_nordic.grib", area="72/-25/54/40")
-
-        for file_name in ["forecast_nordic.grib", "forecast_globe.grib"]:
-            label = file_name.split("_")[-1].split(".")[0]  # gets 'nordic' or 'globe'
-            with xr.open_dataset(file_name, engine="cfgrib") as ds:
-                plot_all_parameters(ds, label)
+        
+        file_name = "forecast_globe.grib" 
+        with xr.open_dataset(file_name, engine="cfgrib") as ds:
+            plot_all_parameters(ds, "global", region="global")
+            plot_all_parameters(ds, "nordic", region="nordic")
 
         # Wait before next update (e.g., 1 hour)
         time.sleep(3600)
